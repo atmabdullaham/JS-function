378440c09bcc2849d0f23580568a70a9751175f4
atmabdullaham <atmabdullaham@gmail.com>
2024-11-15 23:30:52 +0600

    return in a function, when needed and when not

---
+++ argument.js
@@ -0,0 +1,18 @@
+function doubleIt(number) {
+ const doubled = number * 2;
+ console.log(number, doubled);
+}
+
+console.log('I will call the function');
+
+doubleIt(10);
+
+
+// 
+
+function difference(num1, num2) {
+ const diff = num1 - num2;
+}
+
+difference(20, 10)
+
---
+++ return.js
@@ -0,0 +1,18 @@
+// sometimes you need return
+// sometimes no need of return 
+
+function tenTimes(num) {
+ const result = num * 10;
+ return result;
+}
+
+
+function cutHalf(num) {
+ const half = num / 2;
+ return half;
+}
+
+tenTimes(5);
+
+const output = tenTimes(10)
+console.log('output', output);
