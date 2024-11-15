6f7677a74dbcd060944f317afaa87d5976887630
atmabdullaham <atmabdullaham@gmail.com>
2024-11-15 22:43:33 +0600

    first commit

---
+++ function.js
@@ -0,0 +1,21 @@
+const number = 45;
+
+
+function fanOffKor() {
+ console.log('bosa teke ote dare')
+ console.log('wakt to the switch');
+ console.log('click the switch to off of the fan')
+}
+
+
+
+//function name (){
+//}
+// 
+function bruchTeeth() {
+ console.log('pick up the brush')
+}
+
+//call the function 
+fanOffKor();
+bruchTeeth();
